3ad41475ab88716b9bddd68839c0df8d6acceb0a
Oğuzhan <oguzhanatasever93@gmail.com>
2023-03-28 00:49:19 +0300

    primal company

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -1,20 +1,26 @@
 const User = require("../models/user.model");
 const { Company } = require("../models/company.model");
 const { CompanyInviteReq } = require("../models/companyInviteReq.modal");
+const Response = require("../utils/response");
 
 const inviteSomeoneToCompany = async (req, res) => {
-  const email = req.body.email;
-  const user = req.user;
-  const tempUser = await User.find({ email: email });
-  const company = await Company.find({ ownerId: user_id });
-  const companyRequest = CompanyInviteReq({
-    senderId: user._id,
-    companyId: company._id,
-    receiverId: tempUser._id,
-    status: "pending",
-  });
-  await companyRequest.save();
-  return new Response(companyRequest).success(res);
+  try {
+    const email = req.body.email;
+    const user = req.user;
+    const tempUser = await User.findOne({ email: email });
+    const company = await Company.findOne({ ownerId: user._id });
+
+    const companyRequest = new CompanyInviteReq({
+      senderId: user._id,
+      companyId: company._id,
+      receiverId: tempUser._id,
+      status: "pending",
+    });
+    await companyRequest.save();
+    return new Response(companyRequest).success(res);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const pendingCompanyInvite = async (req, res) => {
@@ -22,7 +28,11 @@ const pendingCompanyInvite = async (req, res) => {
     receiverId: req.user._id,
     status: "pending",
   });
-  return new Response(companyInvite).success(res);
+  const messege = `${req.user.name} wants to add you`;
+  return new Response({
+    companyInviteReq: companyInvite,
+    messege: messege,
+  }).success(res);
 };
 
 const responseToInvite = async (req, res) => {
@@ -36,6 +46,9 @@ const responseToInvite = async (req, res) => {
   );
   if (companyInvite && status === "accepted") {
     const company = await Company.findById(companyInvite.companyId);
+    await User.findByIdAndUpdate(req.user._id, {
+      $set: { primalCompany: company._id },
+    });
     company.employees.push(companyInvite.receiverId);
     await company.save();
   }
